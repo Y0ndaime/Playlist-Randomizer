930cce376ac592bc7813302be4150dc1ffb29330
Y0ndaime <45723700+Y0ndaime@users.noreply.github.com>
2020-12-21 01:25:37 +0100

    Added config for amount of playes

--- main.py
+++ main.py
@@ -17,13 +17,20 @@ def create_playlist():
 
 def random_scenario_list():
     scenarioList = []
+    counter = 0
+    amounts = values["entryfields"]
+    repetitions = values["repetitions"]
     for playlistname in config["playlist_names"]:
-        for amount in values["entryfields"]:
-            i = 0
-            while i < int(amount.get()):
-                playlist = json.load(open(path.join(config["playlists_path"], playlistname) + ".json", "r"))
-                scenarioList.append(choice(playlist["scenarioList"]))
-                i += 1
+        amount = amounts[counter]
+        numbers = repetitions[counter].get().split(";")
+        i = 0
+        while i < int(amount.get()):
+            playlist = json.load(open(path.join(config["playlists_path"], playlistname) + ".json", "r"))
+            scen = choice(playlist["scenarioList"])
+            scen["playCount"] = int(numbers[i])
+            scenarioList.append(scen)
+            i += 1
+        counter += 1
     return scenarioList
 
 
@@ -31,34 +38,48 @@ config = json.load(open('config.json', 'r'))
 window = Tk()
 window.title("Playlist Randomizer")
 header = Frame(window)
+labels = Frame(window)
+amount_label = Label(labels, text="Scens")
+repetition_label = Label(labels, text="No. of plays             ")
+name_label = Label(labels, text="Name")
+name_label.pack(side="left")
+repetition_label.pack(side="right")
+amount_label.pack(side="right")
+
 body = Frame(window)
 bottom = Frame(window)
 message = StringVar()
 message_label = Label(bottom, textvariable = message)
 message_label.grid(row=0, column=0, columnspan=2)
 playlist_name_var = StringVar()
-playlist_name_var.set("Name of Playlist")
 playlist_name_entry = Entry(header, textvariable=playlist_name_var)
-playlist_name_entry.setvar("Name of Playlist")
-playlist_name_entry.pack()
+playlist_name_entry.grid(row=0, column=1, sticky="e")
+playlist_name_label = Label(header, text="Name of new Playlist:")
+playlist_name_label.grid(row=0, column=0, sticky="e")
 label_list = []
-entry_list = []
+amount_entries_list = []
+repetitions_entries_list = []
+
 i = 0
 for e in config["playlist_names"]:
     label_list.append(Label(body, text=e))
-    entry_list.append(Entry(body, width=3))
-    entry_list[-1].insert(0, "0")
+    amount_entries_list.append(Entry(body, width=6))
+    amount_entries_list[-1].insert(0, "0")
+    repetitions_entries_list.append(Entry(body, width=12))
     label_list[i].grid(row=i, column=0, sticky="w")
-    entry_list[i].grid(row=i, column=1, sticky="e")
+    amount_entries_list[i].grid(row=i, column=1, sticky="e")
+    repetitions_entries_list[i].grid(row=i, column=2, sticky="e")
     i += 1
 values = dict()
-values["entryfields"] = entry_list
+values["entryfields"] = amount_entries_list
 values["playlistName"] = playlist_name_var
+values["repetitions"] = repetitions_entries_list
 randomize_button = Button(bottom, text="Create Playlist", command=create_playlist)
 randomize_button.grid(row=1, column=0, columnspan=2)
-header.grid(row=0, column=0, columnspan=2)
-body.grid(row=1, column=0, columnspan=2)
-bottom.grid(row=2, column=0, columnspan=2)
+header.pack(fill="x")
+labels.pack(fill="x")
+body.pack(fill="x")
+bottom.pack(fill="x")
 window.mainloop()
 
 
